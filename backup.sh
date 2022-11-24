14e23b9e7bf04a041cbba7961e03d20a41214263
amir <amir.kashapov@protonmail.ch>
2022-11-24 23:45:43 +1100

    feat: tiny backup script

---
+++ backup.sh
@@ -0,0 +1,5 @@
+#crontab -e 
+#0 20 * * * rsync -r /home/amir/Projects/* /media/amir/HDD/Projects
+
+rsync -r /home/amir/Projects/* /media/amir/HDD/Projectsrsync -r /home/amir/Projects/* /media/amir/HDD/Projects
+
