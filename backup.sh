63e5969cbf3635857a0b084398a7d1ffe11626e7
amir <amir.kashapov@protonmail.ch>
2022-11-25 00:48:10 +1100

    fix: typo

--- backup.sh
+++ backup.sh
@@ -1,5 +1,5 @@
 #crontab -e 
 #0 20 * * * rsync -r /home/amir/Projects/* /media/amir/HDD/Projects
 
-rsync -r /home/amir/Projects/* /media/amir/HDD/Projectsrsync -r /home/amir/Projects/* /media/amir/HDD/Projects
+rsync -r /home/amir/Projects/* /media/amir/HDD/Projects
 
